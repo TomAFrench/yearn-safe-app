01b008f2c89374e5e2addd6abadde7d4b12ad45d
Tom French <tom@tomfren.ch>
2020-08-30 02:58:05 +0100

    refactor: cast Erc20Abi to JsonFragment rather than any

--- src/transactions/vaultDespositTxs.ts
+++ src/transactions/vaultDespositTxs.ts
@@ -1,11 +1,10 @@
-import { Interface } from "@ethersproject/abi";
-
+import { Interface, JsonFragment } from "@ethersproject/abi";
 import { BigNumberish } from "@ethersproject/bignumber";
 import { Transaction, VaultAsset } from "../@types";
 import Erc20Abi from "../abis/erc20.json";
 
 const erc20ApproveTx = (vaultAsset: VaultAsset, amount: BigNumberish): Transaction => {
-  const erc20Interface: Interface = new Interface(Erc20Abi as any);
+  const erc20Interface: Interface = new Interface(Erc20Abi as JsonFragment[]);
 
   const approvalTransaction: Transaction = {
     data: erc20Interface.encodeFunctionData("approve", [vaultAsset.vaultContractAddress, amount]),
