9c38a14015801e35e87987faec52c09b160bda99
Tom French <tom@tomfren.ch>
2020-08-30 03:24:44 +0100

    feat: return 0 if getBalance isn't given a proper userAddress

--- src/utils/balance.ts
+++ src/utils/balance.ts
@@ -1,9 +1,11 @@
+import { isAddress } from "@ethersproject/address";
 import { Contract } from "@ethersproject/contracts";
 import { Provider } from "@ethersproject/providers";
 import { BigNumberish } from "@ethersproject/bignumber";
 import Erc20Abi from "../abis/erc20.json";
 
 const getBalance = async (provider: Provider, erc20Address: string, userAddress: string): Promise<BigNumberish> => {
+  if (!isAddress(userAddress)) return "0";
   try {
     if (erc20Address === "Ethereum") return await provider.getBalance(userAddress);
     const erc20Contract = new Contract(erc20Address, Erc20Abi, provider);
