d1a2d0e78b527192e7ccbbcbfcd37d40cdec7804
Tom French <tom@tomfren.ch>
2020-09-04 22:28:02 +0100

    fix: allow withdrawals in ETH rather than WETH
    Previously the ETH vault would return WETH rather than calling withdrawETH

--- src/transactions/vaultWithdrawAllTxs.ts
+++ src/transactions/vaultWithdrawAllTxs.ts
@@ -14,7 +14,25 @@ const vaultWithdrawAllTx = (vaultAsset: VaultAsset): Transaction => {
   return withdrawTransaction;
 };
 
+const vaultWithdrawAllETHTx = (vaultAsset: VaultAsset): Transaction => {
+  const vaultAddress: string = vaultAsset.vaultContractAddress as string;
+  const vaultInterface: Interface = new Interface(vaultAsset.vaultContractABI);
+
+  const withdrawTransaction: Transaction = {
+    data: vaultInterface.encodeFunctionData("withdrawAllETH"),
+    to: vaultAddress,
+    value: "0",
+  };
+
+  return withdrawTransaction;
+};
+
 const vaultWithdrawAllTxs = (vaultAsset: VaultAsset): Transaction[] => {
+  if (vaultAsset.erc20address === "Ethereum") {
+    const withdrawTransaction = vaultWithdrawAllETHTx(vaultAsset);
+    return [withdrawTransaction];
+  }
+
   const withdrawTransaction = vaultWithdrawAllTx(vaultAsset);
 
   return [withdrawTransaction];
--- src/transactions/vaultWithdrawTxs.ts
+++ src/transactions/vaultWithdrawTxs.ts
@@ -16,7 +16,25 @@ const vaultWithdrawTx = (vaultAsset: VaultAsset, amount: BigNumberish): Transact
   return withdrawTransaction;
 };
 
+const vaultWithdrawETHTx = (vaultAsset: VaultAsset, amount: BigNumberish): Transaction => {
+  const vaultAddress: string = vaultAsset.vaultContractAddress as string;
+  const vaultInterface: Interface = new Interface(vaultAsset.vaultContractABI);
+
+  const withdrawTransaction: Transaction = {
+    data: vaultInterface.encodeFunctionData("withdrawETH", [amount]),
+    to: vaultAddress,
+    value: "0",
+  };
+
+  return withdrawTransaction;
+};
+
 const vaultWithdrawTxs = (vaultAsset: VaultAsset, amount: BigNumberish): Transaction[] => {
+  if (vaultAsset.erc20address === "Ethereum") {
+    const withdrawTransaction = vaultWithdrawETHTx(vaultAsset, amount);
+    return [withdrawTransaction];
+  }
+
   const depositTransaction = vaultWithdrawTx(vaultAsset, amount);
 
   return [depositTransaction];
